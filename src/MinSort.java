e6ac71f9479f2ae0232462988d2d1a4c1f0e695d
Kotayss <kotayss30@gmail.com>
2019-07-10 12:57:31 +0300

    Initial commit

---
+++ src/MinSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class MinSort {
+    public static void main(String[] args) {
+        int[] num = {0, 1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(num));
+        int n = num.length;
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                if (num[j] >= num[j + 1]) {
+                    int a = num[j];
+                    num[j] = num[j + 1];
+                    num[j + 1] = a;
+                } else break;
+
+            }
+        }
+        for (int i = 0; i < num.length; i++)
+            System.out.print(num[i] + " ");
+    }
+}
+
+
+
+
+
